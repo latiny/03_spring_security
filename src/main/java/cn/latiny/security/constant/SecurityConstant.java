6d9cf2661c8618ab070b8b6438841673551a8110
liujinwei <liujinwei@bubi.cn>
2020-10-14 18:12:43 +0800

    Add constant variable.

--- src/main/java/cn/latiny/security/constant/SecurityConstant.java
+++ src/main/java/cn/latiny/security/constant/SecurityConstant.java
@@ -19,7 +19,19 @@ public class SecurityConstant {
 
     public static final String SIGN_IN = "/signIn";
 
+    public static final String STATIC_JS = "/**/*.js";
+
+    public static final String STATIC_CSS =  "/**/*.css";
+
+    public static final String STATIC_JPG = "/**/*.jpg";
+
+    public static final String STATIC_PNG =  "/**/*.png";
+
+    public static final String STATIC_ICO =  "/**/*.ico";
+
     public static final String[] NO_LIMITS = {
-            LOGIN_PAGE, LOGIN_OUT, LOGIN_OUT_B, SIGN_IN
+            LOGIN_PAGE, LOGIN_OUT, LOGIN_OUT_B,
+            SIGN_IN, STATIC_JS, STATIC_CSS,
+            STATIC_JPG, STATIC_PNG, STATIC_ICO
     };
 }
